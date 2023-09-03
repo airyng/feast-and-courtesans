5704485e1f8fccce10634a7ef6e52cc1aa3b1c5f
airyng <airyng@yandex.ru>
2023-09-03 12:36:11 +0300

    Removed excess data

--- src/spritesMap.js
+++ src/spritesMap.js
@@ -1,8 +1,5 @@
 export default {
     background: require('./assets/images/background.jpg'),
-    player1_1: require('./assets/images/player.1.1.png'),
-    man1_1: require('./assets/images/man.1.1.png'),
-    woman1_1: require('./assets/images/woman.1.1.png'),
     menSpritesheet: require('./assets/images/spritesheets/men-spritesheet.png'),
     womenSpritesheet: require('./assets/images/spritesheets/women-spritesheet.png')
 }
